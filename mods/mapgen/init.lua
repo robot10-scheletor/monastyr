285e249a8fa6dfc4537d00398c7b5587c6870c00
robot10 <--->
2026-07-28 18:20:50 +0200

    added lava, snow and some biomes

--- mods/mapgen/init.lua
+++ mods/mapgen/init.lua
@@ -1,4 +1,73 @@
 minetest.register_biome({
+    name = "desert",
+
+    -- Strati del terreno emerso
+    node_top = "nodes:sand",
+    depth_top = 2,
+
+    node_filler = "nodes:sand",
+    depth_filler = 3,
+
+    node_stone = "nodes:stone",
+
+    -- NODO DELL'ACQUA
+    node_water = "nodes:saltwater_source",
+    -- ESTENSIONI VERTICALI
+    y_min = -31000,
+    y_max = 50,
+
+    -- PUNTI CLIMATICI
+    heat_point = 100,
+    humidity_point = 0,
+})
+
+minetest.register_biome({
+    name = "glassified_desert",
+
+    -- Strati del terreno emerso
+    node_top = "nodes:glassified_sand",
+    depth_top = 2,
+
+    node_filler = "nodes:glassy_sand",
+    depth_filler = 3,
+
+    node_stone = "nodes:stone",
+
+    -- NODO DELL'ACQUA
+    node_water = "nodes:saltwater_source",
+    -- ESTENSIONI VERTICALI
+    y_min = -5,
+    y_max = 20,
+
+    -- PUNTI CLIMATICI
+    heat_point = 30,
+    humidity_point = 1,
+})
+
+minetest.register_biome({
+    name = "glassified_desert_waters",
+
+    -- Strati del terreno emerso
+    node_top = "nodes:glassy_sand",
+    depth_top = 1,
+
+    node_filler = "nodes:glassy_sand",
+    depth_filler = 2,
+
+    node_stone = "nodes:stone",
+
+    -- NODO DELL'ACQUA
+    node_water = "nodes:saltwater_source",
+    -- ESTENSIONI VERTICALI
+    y_min = -31000,
+    y_max = 0,
+
+    -- PUNTI CLIMATICI
+    heat_point = 30,
+    humidity_point = 1,
+})
+
+minetest.register_biome({
     name = "magnetic_isles",
 
     -- Strati del terreno emerso
@@ -12,13 +81,13 @@ minetest.register_biome({
 
     -- NODO DELL'ACQUA
     node_water = "nodes:saltwater_source",
--- ESTENSIONI VERTICALI
-y_min = 1,
-y_max = 31000,
+    -- ESTENSIONI VERTICALI
+    y_min = 1,
+    y_max = 31000,
 
--- PUNTI CLIMATICI
-heat_point = 100,
-humidity_point = 100,
+    -- PUNTI CLIMATICI
+    heat_point = 100,
+    humidity_point = 100,
 })
 
 minetest.register_biome({
@@ -41,6 +110,28 @@ minetest.register_biome({
     -- PUNTI CLIMATICI
     heat_point = 100,
     humidity_point = 100,
+})
+
+minetest.register_biome({
+    name = "ocean",
+
+    -- Strati del terreno emerso
+    node_top = "nodes:sand",
+    depth_top = 1,
+
+    node_filler = "nodes:sand",
+    depth_filler = 3,
+
+    node_stone = "nodes:stone",
+    -- NODO DELL'ACQUA
+    node_water = "nodes:saltwater_source",
+    -- ESTENSIONI VERTICALI
+    y_min = -31000,
+    y_max = 0,
+
+    -- PUNTI CLIMATICI
+    heat_point = 40,
+    humidity_point = 70,
 })
 
 minetest.register_biome({
@@ -90,13 +181,14 @@ minetest.register_biome({
 })
 
 minetest.register_biome({
-    name = "glassified_desert",
+    name = "snowy_hills",
 
     -- Strati del terreno emerso
-    node_top = "nodes:glassified_sand",
-    depth_top = 2,
+    node_dust = "default:snow",
+    node_top = "nodes:dirt_with_snow",
+    depth_top = 1,
 
-    node_filler = "nodes:glassy_sand",
+    node_filler = "nodes:dirt",
     depth_filler = 3,
 
     node_stone = "nodes:stone",
@@ -104,33 +196,8 @@ minetest.register_biome({
     -- NODO DELL'ACQUA
     node_water = "nodes:saltwater_source",
     -- ESTENSIONI VERTICALI
-    y_min = -5,
-    y_max = 20,
-
-    -- PUNTI CLIMATICI
+    y_min = 1,
+    y_max = 50,
     heat_point = 30,
-    humidity_point = 1,
-})
-
-minetest.register_biome({
-    name = "glassified_desert_waters",
-
-    -- Strati del terreno emerso
-    node_top = "nodes:glassy_sand",
-    depth_top = 1,
-
-    node_filler = "nodes:glassy_sand",
-    depth_filler = 2,
-
-    node_stone = "nodes:stone",
-
-    -- NODO DELL'ACQUA
-    node_water = "nodes:saltwater_source",
-    -- ESTENSIONI VERTICALI
-    y_min = -31000,
-    y_max = 0,
-
-    -- PUNTI CLIMATICI
-    heat_point = 30,
-    humidity_point = 1,
+    humidity_point = 60,
 })
--- mods/nodes/init.lua
+++ mods/nodes/init.lua
@@ -40,6 +40,17 @@ minetest.register_node("nodes:dirt_with_grass", {
     drop = "nodes:dirt 1"
 })
 
+minetest.register_node("nodes:dirt_with_snow", {
+    description = "Dirt With Snow",
+    tiles = {
+        "dirt.png^snowy_grass_top.png",
+        "dirt.png",
+        "dirt.png^snowy_grass_side.png"
+    },
+    groups = {crumbly = 7, soil = 1},
+    drop = "nodes:dirt 1"
+})
+
 minetest.register_node("nodes:glassified_sand", {
     description = "Glassified Sand",
     drawtype = "glasslike",
@@ -48,6 +59,7 @@ minetest.register_node("nodes:glassified_sand", {
     is_ground_content = true,
     use_texture_alpha = "blend",
     paramtype = "light",
+    sunlight_propagates = true
 })
 
 minetest.register_node("nodes:glassy_sand", {
@@ -55,6 +67,76 @@ minetest.register_node("nodes:glassy_sand", {
     tiles = {"glassy_sand.png"},
     groups = {crumbly = 8, soil = 1, falling_node = 1},
     is_ground_content = true,
+})
+
+minetest.register_node("nodes:lava_flowing", {
+    description = "Flowing Lava",
+    drawtype = "flowingliquid",
+    tiles = {"lava_flowing.png"},
+    special_tiles = {
+        {
+            name = "lava_flowing.png",
+            backface_culling = false,
+            animation = {
+                type = "vertical_frames",
+                aspect_w = 32,
+                aspect_h = 32,
+                length = 1.0,
+            },
+        },
+        {
+            name = "lava_flowing.png",
+            backface_culling = true,
+            animation = {
+                type = "vertical_frames",
+                aspect_w = 32,
+                aspect_h = 32,
+                length = 1.0,
+            },
+        },
+    },
+    use_texture_alpha = "blend",
+    paramtype = "light",
+    paramtype2 = "flowingliquid",
+    walkable = false,
+    pointable = false,
+    diggable = false,
+    buildable_to = true,
+    is_ground_content = false,
+    drop = "",
+    drowning = 1,
+    liquidtype = "flowing",
+    liquid_alternative_source = "nodes:lava_source",
+    liquid_alternative_flowing = "nodes:lava_flowing",
+    liquid_viscosity = 1,
+    post_effect_color = {a = 150, r = 10, g = 60, b = 140},
+    groups = {liquid = 3, water = 1, not_in_creative_inventory = 1},
+})
+
+
+minetest.register_node("nodes:lava_source", {
+    description = "Lava Source",
+    drawtype = "liquid",
+    tiles = {
+        {
+            name = "lava.png",
+        },
+    },
+    use_texture_alpha = "blend",
+    paramtype = "light",
+    walkable = false,
+    pointable = false,
+    diggable = false,
+    buildable_to = true,
+    is_ground_content = false,
+    drop = "",
+    drowning = 1,
+    liquidtype = "source",
+    liquid_alternative_source = "nodes:lava_source",
+    liquid_alternative_flowing = "nodes:lava_flowing",
+    liquid_viscosity = 1,
+    post_effect_color = {a = 150, r = 10, g = 60, b = 140},
+    groups = {liquid = 3, water = 1},
 })
 
 minetest.register_node("nodes:magnetite", {
@@ -171,6 +253,35 @@ minetest.register_node("nodes:sand", {
     tiles = {"sand.png"},
     groups = {crumbly=8, soil=1, falling_node = 1},
     is_ground_content = true,
+})
+
+minetest.register_node("nodes:snow", {
+    description = "Snow",
+                       tiles = {"snow.png"},
+                       paramtype = "light",
+                       buildable_to = true,
+                       floodable = true,
+                       drawtype = "nodebox",
+                       node_box = {
+                           type = "fixed",
+                           fixed = {
+                               {-0.5, -0.5, -0.5, 0.5, -0.25, 0.5},
+                           },
+                       },
+                       collision_box = {
+                           type = "fixed",
+                           fixed = {
+                               {-0.5, -0.5, -0.5, 0.5, -6 / 16, 0.5},
+                           },
+                       },
+                       groups = {crumbly = 10, falling_node = 1, snowy = 1},
+
+                       on_construct = function(pos)
+                       pos.y = pos.y - 1
+                       if minetest.get_node(pos).name == "nodes:dirt_with_grass" then
+                           minetest.set_node(pos, {name = "nodes:dirt_with_snow"})
+                           end
+                           end,
 })
 
 minetest.register_node("nodes:stone", {
